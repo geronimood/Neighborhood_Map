1dd3ce12d18f5f3c8ef2a5deaa8e800c2ffd9ea8
Christoph Zeller <zeller.christoph@gmx.de>
2017-12-01 22:15:02 +0100

    Add ko.observables for locationList

--- js/script.js
+++ js/script.js
@@ -1,9 +1,53 @@
 var map;
+
+var initialLocations = [
+  {
+    title: 'Brandenburger Tor',
+    location: {lat: 52.516266, lng: 13.377775}
+  },
+  {
+    title: 'Alexanderplatz',
+    location: {lat: 52.521918, lng: 13.413215}
+  },
+  {
+    title: 'Potsdamer Platz',
+    location: {lat: 52.509663, lng: 13.376481}
+  },
+  {
+    title: 'Mauerpark',
+    location: {lat: 52.544937, lng: 13.402677}
+  },
+  {
+    title: 'Reichstag',
+    location: {lat: 52.518623, lng: 13.376198}
+  }
+];
+
 function initMap() {
   // Constructor creates a new map.
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 52.520008, lng: 13.404954},
     zoom: 13,
     mapTypeControl: false
+  });
+  ko.applyBindings(new ViewModel());
+};
+
+var Location = function(data) {
+  var self = this;
+
+  this.title = data.title;
+  this.location = data.location;
+
+  this.visible = ko.observable(true);
+};
+
+var ViewModel = function() {
+  var self = this;
+
+  this.locationList = ko.observableArray([]);
+
+  initialLocations.forEach(function(locationItem) {
+    self.locationList.push(new Location(locationItem));
   });
 };
